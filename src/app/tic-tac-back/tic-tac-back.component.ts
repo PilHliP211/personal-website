33ba50bb000a3bc20e5ec44847b04b7f6b88520a
Phillip Byram <46680252+PilHliP211@users.noreply.github.com>
2024-07-21 16:12:31 -0500

    add back image with board

--- src/app/tic-tac-back/tic-tac-back.component.ts
+++ src/app/tic-tac-back/tic-tac-back.component.ts
@@ -1,12 +1,14 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-tic-tac-back',
   standalone: true,
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './tic-tac-back.component.html',
   styleUrl: './tic-tac-back.component.scss'
 })
 export class TicTacBackComponent {
 
+  imgSrc = '/back.png';
 }
